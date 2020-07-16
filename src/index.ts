18280f38c1aa6747ac8e989aafe39a8e510e2657
Elton Lobo <elton1231@gmail.com>
2020-07-16 13:21:28 +0400

    use and export single axios instance + export all functions request module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-export { Renovation } from "./renovation";
+export { Renovation, InitParams } from "./renovation";
 export { RenovationBackend } from "./config";
 export * from "./utils";
 export * from "./tests";
-export { GetListParams } from "./model/interfaces";
-export {
-  RenovationError,
-  SessionStatus,
-  RequestResponse,
-  isBrowser,
-  isNodeJS
-} from "./utils/request";
+export * from "./model/interfaces";
+export * from "./utils/request";
